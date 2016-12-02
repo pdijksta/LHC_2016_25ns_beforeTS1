72f332345a3692e61f8835585a0df1517382f87b
LHC scrubbing <lhcscrub@lxplus038.cern.ch>
2016-12-02 16:19:28 +0100

    changed to h5 from pkl

--- 001f_get_all_hl_data.py
+++ 001f_get_all_hl_data.py
@@ -1,24 +1,28 @@
 import sys
 import os
 import cPickle as pickle
+import h5py
 
 import LHCMeasurementTools.LHC_Fills as Fills
 from LHCMeasurementTools.LHC_Fill_LDB_Query import save_variables_and_pickle
 from LHCMeasurementTools.SetOfHomogeneousVariables import SetOfHomogeneousNumericVariables
 
 fill_sublist = [5416]
+attrs = ['timestamps', 'data', 'variables']
 
 variable_file = './GasFlowHLCalculator/variable_list.txt'
-csv_folder = 'fill_all_heatload_data_csvs'
-saved_pkl = csv_folder+'/saved_fills.pkl'
-filepath =  csv_folder+'/all_heatload_data_fill'
+h5_dir = '/eos/user/l/lhcscrub/timber_data_h5/cryo_heat_load_data/'
+saved_pkl = h5_dir+'/saved_fills.pkl'
+file_name = 'cryo_data_fill'
+temp_filepath = './tmp/' + file_name
+temp_file = temp_filepath + '_%i.csv'
+h5_file = h5_dir + file_name + '_%i.h5'
+
 fills_pkl_name = 'fills_and_bmodes.pkl'
-fill_file = filepath + '_%i.csv'
-pkl_file = filepath + '_%i.pkl'
 
 fill_sublist_2 = []
 for fill in fill_sublist:
-    if os.path.isfile(pkl_file % fill):
+    if os.path.isfile(h5_file % fill):
         print('Pkl for fill %i already exists!' % fill)
     else:
         fill_sublist_2.append(fill)
@@ -26,18 +30,18 @@ for fill in fill_sublist:
 with open(variable_file, 'r') as f:
     varlist = f.read().splitlines()[0].split(',')
 
-if not os.path.isdir(csv_folder):
-    os.mkdir(csv_folder)
+if not os.path.isdir(h5_dir):
+    os.mkdir(h5_dir)
 
 with open(fills_pkl_name, 'rb') as fid:
     dict_fill_bmodes = pickle.load(fid)
 
-save_variables_and_pickle(varlist=varlist, file_path_prefix=filepath, 
+save_variables_and_pickle(varlist=varlist, file_path_prefix=temp_filepath,
                           save_pkl=saved_pkl, fills_dict=dict_fill_bmodes, fill_sublist=fill_sublist_2)
 
 for filln in fill_sublist_2:
-    homogeneous_ob = SetOfHomogeneousNumericVariables(varlist, fill_file % filln)
-    atd_ob = homogeneous_ob.aligned_object()
-    with open(pkl_file % filln, 'w') as f:
-        pickle.dump(atd_ob, f, protocol=-1)
-    os.remove(fill_file % filln)
+    htd_ob = SetOfHomogeneousNumericVariables(varlist, temp_file % filln).aligned_object()
+    with open(h5_file % filln, 'w') as h5_handle:
+        for attr in attrs:
+            h5_handle.create_dataset(attr, data=getattr(htd_ob, attr))
+    os.remove(temp_file % filln)
