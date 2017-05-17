3c113c8be85c68738d2e1e42207728ea15725c43
LHC scrubbing <lhcscrub@lxplus030.cern.ch>
2017-05-17 09:16:46 +0200

    optionally plot beam modes

--- 016_heat_load_arcs_vs_SAMs.py
+++ 016_heat_load_arcs_vs_SAMs.py
@@ -30,6 +30,10 @@ parser.add_argument('--noaverage', help='Do not show an average heat load.', act
 parser.add_argument('--fbct', help='Show fbct intensity, too!', action='store_true')
 parser.add_argument('--noplotmodel', help='Do not plot the model heat load', action='store_true')
 parser.add_argument('--savefig', help='Save figures in pdijksta dir', action='store_true')
+parser.add_argument('--beam-events', help='Show when is begin of squeeze etc.', action = 'store_true')
+parser.add_argument('--use-recalc', help='Recalculated heat loads from Gasflow.', action = 'store_true')
+parser.add_argument('-v', help='Verbose parsing of timber files.', action = 'store_true')
+parser.add_argument('--time', metavar='TIME', type=str, nargs='*')
 args = parser.parse_args()
 
 filln = args.filln
@@ -38,9 +42,10 @@ flag_bunch_length = not args.noblength
 flag_average = not args.noaverage
 flag_fbct = args.fbct
 plot_model = not args.noplotmodel
+plot_t_arr = True
+use_recalculated = args.use_recalc
 
-myfontsz = 16
-use_recalculated = True
+myfontsz = 14
 pl.close('all')
 ms.mystyle_arial(fontsz=myfontsz, dist_tick_lab=8)
 
@@ -81,12 +86,12 @@ with open('fills_and_bmodes.pkl', 'rb') as fid:
     dict_fill_bmodes = pickle.load(fid)
 
 fill_dict = {}
-fill_dict.update(tm.parse_timber_file('fill_basic_data_csvs/basic_data_fill_%d.csv'%filln, verbose=False))
-fill_dict.update(tm.parse_timber_file('fill_bunchbybunch_data_csvs/bunchbybunch_data_fill_%d.csv'%filln, verbose=False))
+fill_dict.update(tm.parse_timber_file('fill_basic_data_csvs/basic_data_fill_%d.csv'%filln, verbose=args.v))
+fill_dict.update(tm.parse_timber_file('fill_bunchbybunch_data_csvs/bunchbybunch_data_fill_%d.csv'%filln, verbose=args.v))
 if use_recalculated:
     fill_dict.update(qf.get_fill_dict(filln))
 else:
-    fill_dict.update(tm.parse_timber_file('fill_heatload_data_csvs/heatloads_fill_%d.csv'%filln, verbose=False))
+    fill_dict.update(tm.parse_timber_file('fill_heatload_data_csvs/heatloads_fill_%d.csv'%filln, verbose=args.v))
 
 dict_beam = fill_dict
 dict_fbct = fill_dict
@@ -118,7 +123,9 @@ group_names = dict_hl_groups.keys()
 N_figures = len(group_names)
 figs = []
 sp1 = None
-for ii in xrange(N_figures):
+
+
+for ii, group_name in enumerate(group_names):
     fig_h = pl.figure(ii, figsize=(12, 10))
     figs.append(fig_h)
     fig_h.patch.set_facecolor('w')
@@ -130,14 +137,27 @@ for ii in xrange(N_figures):
     sphlcell = pl.subplot(3,1,2, sharex=sp1)
     spenergy = sptotint.twinx()
 
-    spenergy.plot((energy.t_stamps-t_ref)/3600., energy.energy/1e3, c='black', lw=2.)#, alpha=0.1)
+    spenergy.plot((energy.t_stamps-t_ref)/3600., energy.energy/1e3, c='black', lw=2., label='Energy')
     spenergy.set_ylabel('Energy [TeV]')
     spenergy.set_ylim(0,7)
+
+    if args.beam_events:
+            keys_labels = [('t_start_INJPHYS', 'Injection'),
+                    ('t_start_RAMP',    'Ramp'),
+                    ('t_start_FLATTOP', 'Flat top'),
+                    ('t_start_SQUEEZE', 'Squeeze'),
+                    ('t_start_ADJUST',  'Adjust'),
+                    ('t_start_STABLE',  'Stable beams')
+                    ]
+            for ctr, (key, label) in enumerate(keys_labels):
+                color = ms.colorprog(ctr, len(keys_labels)+1)
+                tt = (dict_fill_bmodes[filln][key] - t_ref)/3600.
+                spenergy.axvline(tt, ls='-', color=color, label=label)
 
     for beam_n in beams_list:
 
         if flag_fbct: sptotint.plot((fbct_bx[beam_n].t_stamps-t_ref)/3600., fbct_bx[beam_n].totint, '.--', color=colstr[beam_n])
-        sptotint.plot((bct_bx[beam_n].t_stamps-t_ref)/3600., bct_bx[beam_n].values, '-', color=colstr[beam_n], lw=2.)
+        sptotint.plot((bct_bx[beam_n].t_stamps-t_ref)/3600., bct_bx[beam_n].values, '-', color=colstr[beam_n], lw=2., label='Intensity B%i' % beam_n)
         sptotint.set_ylabel('Total intensity [p+]')
         sptotint.grid('on')
 
@@ -148,8 +168,14 @@ for ii in xrange(N_figures):
             spavbl.grid('on')
             spavbl.set_xlabel('Time [h]')
 
+    ms.comb_legend(sptotint, spenergy, bbox_to_anchor=(1.1, 1),  loc='upper left', prop={'size':myfontsz})
+
     group_name = group_names[ii]
-    pl.suptitle(' Fill. %d started on %s\n%s'%(filln, tref_string, group_name))
+    if use_recalculated:
+	string = 'Recalculated data'
+    else:
+	string = 'Logged data'
+    pl.suptitle(' Fill. %d started on %s\n%s (%s)'%(filln, tref_string, group_name, string))
     fig_h.canvas.set_window_title(group_name)
 
     hl_var_names = dict_hl_groups[group_name][:]
@@ -161,7 +187,7 @@ for ii in xrange(N_figures):
     heatloads = SetOfHomogeneousNumericVariables(variable_list=hl_var_names, timber_variables=fill_dict)
 
     # CORRECT ARC AVERAGES
-    if group_name == 'Arcs' and filln < first_correct_filln:
+    if not use_recalculated and group_name == 'Arcs' and filln < first_correct_filln:
         hl_corr_factors = []
         for ii, varname in enumerate(dict_hl_groups[group_name]):
             if varname not in blacklist:
@@ -194,7 +220,7 @@ for ii in xrange(N_figures):
 
         hl_imped_fill = fc.HeatLoad_calculated_fill(fill_dict, hli_calculator, bct_dict=bct_bx, fbct_dict=fbct_bx, blength_dict=blength_bx)
         hl_sr_fill = fc.HeatLoad_calculated_fill(fill_dict, hlsr_calculator, bct_dict=bct_bx, fbct_dict=fbct_bx, blength_dict=blength_bx)
-        label='Imp.+SR\n(recalc.)'
+        label='Imp.+SR (recalc.)'
         sphlcell.plot((hl_imped_fill.t_stamps-t_ref)/3600,
             (hl_imped_fill.heat_load_calculated_total+hl_sr_fill.heat_load_calculated_total)*HL.magnet_length['AVG_ARC'][0],
             '--', color='grey', lw=2., label=label)
@@ -213,7 +239,7 @@ for ii in xrange(N_figures):
     sphlcell.set_ylabel('Heat load [W]')
 
     #~ sphlcell.set_xlabel('Time [h]')
-    sphlcell.legend(prop={'size':myfontsz}, bbox_to_anchor=(0.95, 1),  loc='upper left')
+    sphlcell.legend(prop={'size':myfontsz}, bbox_to_anchor=(1.1, 1.0),  loc='upper left')
     sphlcell.grid('on')
 
     pl.subplots_adjust(right=0.65, wspace=0.35)
